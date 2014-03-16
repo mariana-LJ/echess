1198b7f3b380be163e9e54c2c5de596a35062c07
mlj <mlj@marypc.(none)>
2014-03-16 14:55:41 -0500

    Create class for A2D IC. Read both A2Ds.

--- a2dTest/a2dTest.cpp
+++ a2dTest/a2dTest.cpp
@@ -13,61 +13,94 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <iostream>
+#include <string>
 
 typedef unsigned char byte;
+using namespace std;
+
+class A2D_Converter {
+public:
+	A2D_Converter(const char * fileName, byte a2d_address) {
+		fileName_ = fileName;
+		a2d_address_ = a2d_address;
+		fileDescr_ = 0;
+	}
+
+	void Initialize() {
+		if ((fileDescr_ = open(fileName_, O_RDWR)) < 0) {		// Open port for reading and writing
+			printf("Failed to open i2c port\n");
+			exit(1);
+		}
+
+		// Set the port options and set the address of the device we wish to speak to:
+		if (ioctl(fileDescr_, I2C_SLAVE, a2d_address_) < 0) {
+			printf("Unable to get bus access to talk to slave\n");
+			exit(1);
+		}
+
+		buf_[0] = 0x00; // Set A/D to read channel 0
+		if ((write(fileDescr_, buf_, 1)) != 1) {		// Send control byte
+				printf("Error writing to i2c slave\n");
+				exit(1);
+		}
+
+		buf_[0] = 0x44; // Set A/D to auto increment mode
+		if ((write(fileDescr_, buf_, 1)) != 1) {		// Send control byte
+				printf("Error writing to i2c slave\n");
+				exit(1);
+		}
+
+		if((read(fileDescr_, buf_, 1)) != 1){				// Read previously stored value (possibly garbage)
+			printf("Error reading from i2c slave\n");
+			exit(1);
+		}
+	}
+
+	void Read() {
+		if ((read(fileDescr_, buf_, 4)) != 4) {		// Read 4 bytes from A/D
+			printf("Error reading from i2c slave\n");
+			exit(1);
+		}
+	}
+
+	byte Get(int channel) {
+		return buf_[channel];
+	}
+
+private:
+	const char * fileName_;
+	int fileDescr_;         // File description
+	byte a2d_address_;      // I2C Address of PF8591 Chip
+	byte buf_[4];  // Buffer for data being read/ written on the i2c bus
+};
+
 
 int main(int argc, char **argv)
 {
-
-	int fileDescr;							// File description
-	char *fileName = "/dev/i2c-1";			// Name of the port we will be using (1 for Rev. 2, 0-Rev.1)
-	const byte  a2d_address=0x48;      		// I2C Address of PF8591 Chip
-	unsigned char buf[10];					// Buffer for data being read/ written on the i2c bus
 	int i = 0;								// Auxiliary
 	int b = 0;
 	struct timespec tim, tim2;				// Nanosleep function setup
 	tim.tv_sec = 0;
 	tim.tv_nsec = 500000000L;
+	string filename = "/dev/i2c-1";
 
-	if ((fileDescr = open(fileName, O_RDWR)) < 0) {		// Open port for reading and writing
-		printf("Failed to open i2c port\n");
-		exit(1);
-	}
-
-	// Set the port options and set the address of the device we wish to speak to:
-	if (ioctl(fileDescr, I2C_SLAVE, a2d_address) < 0) {
-		printf("Unable to get bus access to talk to slave\n");
-		exit(1);
-	}
-
-	buf[0] = 0x00; // Set A/D to read channel 0
-	if ((write(fileDescr, buf, 1)) != 1) {		// Send control byte
-			printf("Error writing to i2c slave\n");
-			exit(1);
-	}
-
-	buf[0] = 0x44; // Set A/D to auto increment mode
-	if ((write(fileDescr, buf, 1)) != 1) {		// Send control byte
-			printf("Error writing to i2c slave\n");
-			exit(1);
-	}
-
-	if((read(fileDescr, buf, 1)) != 1){				// Read previously stored value (possibly garbage)
-		printf("Error reading from i2c slave\n");
-		exit(1);
-	}
+	A2D_Converter a2d_1(filename.c_str(), 0x48);
+	A2D_Converter a2d_2(filename.c_str(), 0x49);
+	a2d_1.Initialize();
+	a2d_2.Initialize();
 
 	for(i = 0; i<100; i++) {
-		//system("clear");
-		if ((read(fileDescr, buf, 4)) != 4) {		// Read 5 bytes from A/D
-			printf("Error reading from i2c slave\n");
-			exit(1);
-		} else {
-			for(b = 0; b<4; b++){
-				printf("%x  ",buf[b]); // Print last byte (5th byte)
-			}
-			printf("\n");
+		a2d_1.Read();
+		a2d_2.Read();
+
+		for(b = 0; b<4; b++){
+			printf("%x ", a2d_1.Get(b));
 		}
+		for(b = 0; b<4; b++){
+			printf("%x ", a2d_2.Get(b));
+		}
+		printf("\n");
 
 		if(nanosleep(&tim , &tim2) < 0 ){
 			  printf("Nano sleep system call failed \n");
